ab58889f67683c17e9f04cf918f1b52033beaec2
hungyanbin <hung_yanbin@hotmail.com>
2022-01-02 16:50:56 +0800

    Show KalendarView by month

---
+++ app/src/main/java/com/yanbin/kalendar/KalendarRange.kt
@@ -0,0 +1,30 @@
+package com.yanbin.kalendar
+
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.Month
+import java.time.temporal.ChronoUnit
+import kotlin.math.ceil
+
+class KalendarRange(
+    val beginDate: LocalDate,
+    val numberOfRow: Int,
+    val currentMonth: Month?
+) {
+    companion object {
+        fun ofMonth(year: Int, month: Month): KalendarRange {
+            val firstDayOfWeek = DayOfWeek.SUNDAY.value
+            val firstDayOfMonth = LocalDate.of(year, month, 1)
+            val weekDayOfFirstDay = firstDayOfMonth.dayOfWeek.value
+            val firstDayOffset = (weekDayOfFirstDay - firstDayOfWeek + 7) % 7
+            val beginDate = firstDayOfMonth.minusDays(firstDayOffset.toLong())
+            val totalDays = ChronoUnit.DAYS.between(beginDate, firstDayOfMonth.plusMonths(1)) - 1
+
+            return KalendarRange(
+                beginDate,
+                ceil(totalDays.toFloat()/7).toInt(),
+                month
+            )
+        }
+    }
+}
--- app/src/main/java/com/yanbin/kalendar/KalendarView.kt
+++ app/src/main/java/com/yanbin/kalendar/KalendarView.kt
@@ -6,15 +6,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.yanbin.kalendar.ui.theme.KalendarTheme
 import java.time.LocalDate
+import java.time.Month
 
 @Composable
 fun KalendarView(
     modifier: Modifier = Modifier,
     kalendarRange: KalendarRange,
-    dayView: @Composable (LocalDate, Modifier) -> Unit
+    dayView: @Composable (LocalDate, Month?, Modifier) -> Unit
 ) {
     val columnCount = 7
     Column(modifier = modifier) {
@@ -22,34 +22,44 @@ fun KalendarView(
             Row(
                 modifier = Modifier.fillMaxWidth()
             ) {
-                for(column in 0..columnCount) {
+                for(column in 0 until columnCount) {
                     val date = kalendarRange.beginDate.plusDays((column + columnCount * row).toLong())
-                    dayView(date, Modifier.weight(1f))
+                    dayView(date, kalendarRange.currentMonth, Modifier.weight(1f))
                 }
             }
         }
     }
 }
 
-class KalendarRange(
-    val beginDate: LocalDate,
-    val numberOfRow: Int
-)
-
-@Preview(showBackground = true)
 @Composable
-fun KalendarViewPreview(){
-    KalendarTheme {
-        KalendarView(
-            modifier = Modifier.height(100.dp),
-            kalendarRange = KalendarRange(LocalDate.now(), 4),
-            dayView = { localDate, modifier ->
+fun DefaultKalendarView(
+    modifier: Modifier = Modifier,
+    kalendarRange: KalendarRange
+) {
+    KalendarView(
+        modifier = modifier,
+        kalendarRange = kalendarRange,
+        dayView = { localDate, month, modifier ->
+            if (localDate.month != month) {
+                Spacer(modifier = modifier)
+            } else {
                 Text(
                     text = localDate.dayOfMonth.toString(),
                     modifier = modifier,
                     textAlign = TextAlign.Center
                 )
             }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun KalendarViewPreview(){
+    KalendarTheme {
+        DefaultKalendarView(
+            modifier = Modifier.fillMaxHeight(),
+            kalendarRange = KalendarRange.ofMonth(2022, Month.FEBRUARY),
         )
     }
 }
