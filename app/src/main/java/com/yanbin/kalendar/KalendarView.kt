6ff192754422f0187303e7426c46c8c10aa9a9a9
hungyanbin <hung_yanbin@hotmail.com>
2022-01-02 17:06:49 +0800

    Finish single selection

--- app/src/main/java/com/yanbin/kalendar/KalendarView.kt
+++ app/src/main/java/com/yanbin/kalendar/KalendarView.kt
@@ -1,9 +1,13 @@
 package com.yanbin.kalendar
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.yanbin.kalendar.ui.theme.KalendarTheme
@@ -36,21 +40,48 @@ fun DefaultKalendarView(
     modifier: Modifier = Modifier,
     kalendarRange: KalendarRange
 ) {
+    var selectedDate by remember {
+        mutableStateOf(kalendarRange.beginDate)
+    }
+
     KalendarView(
         modifier = modifier,
         kalendarRange = kalendarRange,
-        dayView = { localDate, month, modifier ->
-            if (localDate.month != month) {
-                Spacer(modifier = modifier)
-            } else {
-                Text(
-                    text = localDate.dayOfMonth.toString(),
-                    modifier = modifier,
-                    textAlign = TextAlign.Center
-                )
-            }
+        dayView = { localDate, month, dayViewModifier ->
+            val selected = localDate == selectedDate
+            DefaultDayView(
+                modifier = dayViewModifier,
+                date = localDate,
+                month = month,
+                selected = selected,
+                onClicked = {
+                    selectedDate = it
+                }
+            )
         }
     )
+}
+
+@Composable
+fun DefaultDayView(
+    modifier: Modifier,
+    date: LocalDate,
+    month: Month? = null,
+    selected: Boolean = false,
+    onClicked: (LocalDate) -> Unit = {}
+) {
+    val backgroundColor = if(selected) Color.Cyan else Color.Transparent
+    Surface(
+        modifier = modifier.clickable { onClicked(date) },
+        color = backgroundColor
+    ) {
+        if (date.month == month) {
+            Text(
+                text = date.dayOfMonth.toString(),
+                textAlign = TextAlign.Center
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/yanbin/kalendar/MainActivity.kt
+++ app/src/main/java/com/yanbin/kalendar/MainActivity.kt
@@ -3,12 +3,15 @@ package com.yanbin.kalendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.yanbin.kalendar.ui.theme.KalendarTheme
+import java.time.Month
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,22 +20,12 @@ class MainActivity : ComponentActivity() {
             KalendarTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    DefaultKalendarView(
+                        modifier = Modifier.fillMaxHeight(),
+                        kalendarRange = KalendarRange.ofMonth(2022, Month.JANUARY),
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    KalendarTheme {
-        Greeting("Android")
     }
 }
